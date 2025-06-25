3fe2aa4566e186cbf558ccf442b2a1289a233f1d
jasmine556694 <1047947139@qq.com>
2025-06-25 17:56:56 +0800

    Fix: Update tailwind.config.js to scan all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./about.html",
-    "./blog-detail.html",
-    "./blog.html",
-    "./contact.html",
-    "./project-detail.html",
-    "./projects.html",
-    "./js/*.js" // 如果JavaScript文件中也使用了Tailwind类，需要添加此行
+    './*.html',
+    './js/**/*.js',
   ],
   theme: {
     extend: {},
